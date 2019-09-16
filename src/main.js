f703d9fd441e45d20f575f78b8017c357fa17415
Jan Stabenow <moin@jan-stabenow.de>
2019-09-16 16:20:52 +0200

    Fix hide on 'MEDIACONTROL_HIDE'

--- src/main.js
+++ src/main.js
@@ -117,7 +117,7 @@ export default class InfoButton extends UICorePlugin {
 		this.toggleContextMenu();
 	}
 
-	hideSelectLevelMenu() {
+	hideInfoButtonMenu() {
 		this.$('.info_button ul').hide();
 	}
 
